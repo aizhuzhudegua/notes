ddbf638865e5fd8c2f052ada4041a2457e3b6aaf
aizhuzhudegua <1648476050@qq.com>
2023-12-05 21:50:29 +0800

    successfully build the environment and a demo based on opengl.

--- game/game/src/game.cpp
+++ game/game/src/game.cpp
@@ -1,59 +1,44 @@
-﻿// game.cpp: 定义应用程序的入口点。
-//
-
-#include "game.h"
-
+﻿
+#define GlEW_STATIC 
+#include <game.h>
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
 using namespace std;
 
-//main.cpp
-#include <glad/glad.h>
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
 int main(void)
 {
-    float vertices[] = {
-        0.5f,0.5f,0.0f,  //右上
-        0.5f,-0.5f,0.0f, //右下
-        -0.5f,-0.5f,0.0f,//左下
-        -0.5f,0.5f,0.0f //左上
-    };
+    //初始化GLFW函数库
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    unsigned int indices[] = {
-        0,1,3, //第一个三角形
-        1,2,3 //第二个三角形
-    };
+    //Open GLFW Window
+    GLFWwindow* window = glfwCreateWindow(800,600,"myopengl",NULL,NULL);
+    if (window==NULL)
+    {
+        cout << "open window failed!" << endl;
+        //关闭GLFW函数库
+        glfwTerminate();
+        return -1;
+    }
 
-    //生成立方体的VAO、VBO和EBO
-    unsigned int VAO, VBO, EBO;
-    glGenVertexArrays(1,&VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+    glfwMakeContextCurrent(window);
 
-    //绑定VAO
-    glBindVertexArray(VAO);
-    //绑定VBO并传入顶点数据
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    //绑定EBO并传入索引数据
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    //设置顶点属性指针
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
-    glEnableVertexAttribArray(0);
-    //解绑VBO
-    glBindBuffer(GL_ARRAY_BUFFER,0);
-    //注意不要解绑EBO，因为EBO存储在VAO中
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
-    //解绑VAO
-    glBindVertexArray(0);
+    //Init glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
-    //渲染时
-    //使用之前连接好的着色器程序
-    glUseProgram(glShaderProgram);
-    //绑定VAO(实际上不需要每次绑定)
-    glBindVertexArray(VAO);
-    //用EBO绘制四边形
-    glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+    glViewport(0,0,800,600);
 
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
     return 0;
 }
--- opengl/OpenGL.md
+++ opengl/OpenGL.md
@@ -1,5 +1,19 @@
 # OpenGL
 
-https://glew.sourceforge.net/
+## 手动安装
 
-https://www.glfw.org/download.html
+- cmake
+
+- https://glew.sourceforge.net/
+
+- https://www.glfw.org/download.html
+
+- **opengl32.lib**在C:\Windows\System32下，但是没必要特地引入，用find_package()找
+
+- 学习文档https://learnopengl.com/
+
+- 目录结构
+
+  ![image-20231205161732119](images/image-20231205161732119.png)
+
+## Nuget在线安装（未尝试）
